c126e32ad49f60089bff964b3e75662c2278f3ff
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-07-05 08:58:15 +0100

    Refactor database connection to use environment variables

--- db.js
+++ db.js
@@ -1,13 +1,20 @@
 const {Sequelize} = require('sequelize')
-const config = require('./config/config')
+const dotenv = require("dotenv")
 
+dotenv.config()
 
-const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password,{
-    host: config.development.host,
-    port: config.development.port,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
+    host:process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect:'postgres',
 })
 
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }
 // (async () => {
 //     try {
 //         await sequelize.authenticate();
@@ -16,6 +23,14 @@ const sequelize = new Sequelize(config.development.database, config.development.
 //         console.error('Unable to connect to the database:', error);
 //     }
 // })()
+async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
 
 module.exports = sequelize
 
