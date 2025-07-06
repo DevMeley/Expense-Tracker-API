f685c3665b9f98656476d7b88b936f293d1e7bdc
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-07-06 11:18:58 +0100

    Refactor db.js for improved database initialization and logging; update package.json to streamline server start command

--- db.js
+++ db.js
@@ -11,24 +11,21 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false,
     },
   },
-  logging: false,
+  logging: false 
 });
 
-module.exports = sequelize;
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
-//     host:process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialect:'postgres',
-// })
-
-async () => {
+const initializeDatabase = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+    await sequelize.sync({ alter: true });
+    console.log("Database synced successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 };
+
+initializeDatabase();
 
 module.exports = sequelize;
