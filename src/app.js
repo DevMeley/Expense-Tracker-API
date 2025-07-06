1f13e18fb67958dafa7715df46700051b7145d00
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-07-06 10:55:31 +0100

    Refactor app.js for improved code consistency and formatting

--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const sharedRouter = require('../Routes/v1/sharedRoutes')
-const userRouter = require('../Routes/v1/userRoutes')
-const categoryRouter = require('../Routes/v1/categoryRoutes')
-const expenseRouter = require('../Routes/v1/expenseRoutes')
-const budgetRouter = require('../Routes/v1/budgetRoute')
-const NotificationRouter = require('../Routes/v1/notificationRoute')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const sharedRouter = require("../Routes/v1/sharedRoutes");
+const userRouter = require("../Routes/v1/userRoutes");
+const categoryRouter = require("../Routes/v1/categoryRoutes");
+const expenseRouter = require("../Routes/v1/expenseRoutes");
+const budgetRouter = require("../Routes/v1/budgetRoute");
+const NotificationRouter = require("../Routes/v1/notificationRoute");
 
 // load the environmental variable from the env files
-dotenv.config()
+dotenv.config();
 
-const app = express()
-
+const app = express();
 
 // middleware
-app.use(express.json())
-// app.use(cors(corsOptions))
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "100.20.92.101",
+      "44.225.181.72",
+      "44.227.217.144",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // routes
-app.use('/v1/shared', sharedRouter)
-app.use('/v1/user', userRouter)
-app.use('/v1/categories', categoryRouter)
-app.use('/v1/expenses', expenseRouter)
-app.use('/v1/budgetlimit', budgetRouter)
-app.use('/v1/notification', NotificationRouter)
+app.use("/v1/shared", sharedRouter);
+app.use("/v1/user", userRouter);
+app.use("/v1/categories", categoryRouter);
+app.use("/v1/expenses", expenseRouter);
+app.use("/v1/budgetlimit", budgetRouter);
+app.use("/v1/notification", NotificationRouter);
 
-
-
-module.exports = app
+module.exports = app;
