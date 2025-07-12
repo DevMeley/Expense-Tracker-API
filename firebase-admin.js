237c7fc0d9a6e8b59c6042c012a55b5ce5313861
Ayantola Abisola Monsurat <ayantolamonsurat@gmail.com>
2025-07-12 20:43:06 +0100

    Update Firebase admin SDK configuration with correct service account path and add service account JSON file

--- firebase-admin.js
+++ firebase-admin.js
@@ -1,9 +1,8 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./path/to/serviceAccountKey.json');
+const serviceAccount = require('./expense-tracker-7149f-firebase-adminsdk-fbsvc-ebfa3fb63d.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // Add other config if needed
 });
 
 module.exports = admin;
